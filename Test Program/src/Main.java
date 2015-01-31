e5693d5e2d38923e8450bdc8a9681f9c4de2f33c
eubankct <eubankct@rose-hulman.edu>
2015-01-31 15:46:44 -0500

    adjusted GridBagLayout to correctly size components

--- Test Program/src/Main.java
+++ Test Program/src/Main.java
@@ -1,54 +1,55 @@
 import javax.swing.*;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
+
+import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		JFrame mainWindow = new JFrame("ATE Simulator");
-		mainWindow.setSize(1024, 768);
-		GridBagLayout mainWindowManager = new GridBagLayout();
-		ArrayList<String> dataLines = new ArrayList<String>();
-		JTabbedPane programStepTabs = new JTabbedPane();
-		ArrayList<ProgramStepPanel> programSteps = new ArrayList<ProgramStepPanel>();
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container mainPane = mainWindow.getContentPane();
+		GridBagLayout mainPaneLayout = new GridBagLayout();
+		mainPane.setLayout(mainPaneLayout);
 		
-		mainWindow.setLayout(mainWindowManager);
+		JTabbedPane programStepTabs = new JTabbedPane();
+		//ArrayList<ProgramStepPanel> programSteps = new ArrayList<ProgramStepPanel>();
 		
 		JTextPane programDisplay = new JTextPane();
 		GridBagConstraints programDisplayConstraints = new GridBagConstraints();
 		programDisplayConstraints.gridx = 0;
 		programDisplayConstraints.gridy = 0;
+		programDisplayConstraints.weighty = 1.0;
 		programDisplayConstraints.fill = GridBagConstraints.BOTH;
-		mainWindowManager.setConstraints(programDisplay, programDisplayConstraints);
+		programDisplay.setMinimumSize(new Dimension(400, 768));
+		programDisplay.setPreferredSize(new Dimension(400, 768));
 		programDisplay.setEditable(false);
 		
 		GridBagConstraints programStepTabsConstraints = new GridBagConstraints();
 		programStepTabsConstraints.gridx = 1;
 		programStepTabsConstraints.gridy = 0;
+		programStepTabsConstraints.weightx = 1.0;
+		programStepTabsConstraints.weighty = 1.0;
 		programStepTabsConstraints.fill = GridBagConstraints.BOTH;
-		mainWindowManager.setConstraints(programStepTabs, programStepTabsConstraints);
 		
 		JButton newTabButton = new JButton("+");
 		GridBagConstraints newTabButtonConstraints = new GridBagConstraints();
 		newTabButtonConstraints.gridx = 2;
 		newTabButtonConstraints.gridy = 0;
-		newTabButtonConstraints.weightx = 0.0;
-		newTabButtonConstraints.weighty = 0.0;
-		mainWindowManager.setConstraints(newTabButton, newTabButtonConstraints);
 		newTabButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				programSteps.add(new ProgramStepPanel());
-				programStepTabs.add("Step " + programStepTabs.getTabCount(), programSteps.get(programStepTabs.getTabCount()));
+				//programSteps.add(new ProgramStepPanel());
+				//programStepTabs.add("Step " + programStepTabs.getTabCount(), programSteps.get(programStepTabs.getTabCount()));
+				programStepTabs.add("Step " + programStepTabs.getTabCount(), new ProgramStepPanel());
 			}
 		});
 		
-		mainWindow.add(programDisplay);
-		mainWindow.add(programStepTabs);
-		mainWindow.add(newTabButton);
+		mainPane.add(programDisplay, programDisplayConstraints);
+		mainPane.add(programStepTabs, programStepTabsConstraints);
+		mainPane.add(newTabButton, newTabButtonConstraints);
 		
+		mainWindow.setSize(1024, 768);
 		mainWindow.setVisible(true);
 	}
 
